04dd8abf3772a0ac32a23b8884a9d387f60c701c
Andrew Burnes <apburnes@gmail.com>
2015-08-20 18:09:04 -0600

    Add convert shapefile to geojson

---
+++ test/convertShp.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+var expect = require('chai').expect;
+var geojsonhint = require('geojsonhint');
+var convertShp = require('../lib/convertShp');
+
+var blockGroups = path.join(__dirname, './fixtures/block_groups.shp');
+var badBlockGroups = path.join(__dirname, './fixtures/not_a_shape.shp');
+var origProps = [
+  'STATEFP',
+  'COUNTYFP',
+  'TRACTCE',
+  'BLKGRPCE',
+  'GEOID',
+  'NAMELSAD',
+  'MTFCC',
+  'FUNCSTAT',
+  'ALAND',
+  'AWATER',
+  'INTPTLAT',
+  'INTPTLON'
+];
+var omitProps = origProps.slice(5, origProps.length);
+var selectProps = origProps.slice(0, 5);
+var featureCount = 2505;
+
+describe('Converting a shapefile to geojson', function() {
+  it('should convert a shapefile to an array of features and keep original the properties', function(done) {
+    convertShp(blockGroups, function(err, features) {
+      var after = _.after(features.length, function() {
+        return done(err);
+      });
+
+      expect(features.length).to.be.equal(featureCount);
+
+      _.forEach(features, function(feature) {
+        expect(geojsonhint.hint(feature)).to.be.empty;
+        expect(feature.properties).to.have.keys(origProps)
+        return after();
+      });
+    });
+  });
+
+  it('should convert a shapefile to an array of features and omit selected properties', function(done) {
+    convertShp(blockGroups, omitProps, function(err, features) {
+      var after = _.after(features.length, function() {
+        return done(err);
+      });
+
+      expect(features.length).to.be.equal(featureCount);
+
+      _.forEach(features, function(feature) {
+        expect(geojsonhint.hint(feature)).to.be.empty;
+        expect(feature.properties).to.have.keys(selectProps)
+        return after();
+      });
+    });
+  });
+
+  it('should reject with an invalid shapefile', function(done) {
+    convertShp(badBlockGroups, function(err, features) {
+      expect(err).to.be.instanceof(Error);
+      done(features);
+    });
+  });
+});
