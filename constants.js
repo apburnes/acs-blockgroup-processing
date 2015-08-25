502c6b9dae6889bc6d7da3a6245cf2c5cd658f1b
Andrew Burnes <apburnes@gmail.com>
2015-08-24 20:10:39 -0600

    Initial pointifyBlocks functionality

---
+++ constants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports.raceKeys = {
+  HD01_VD01: 'total_pop',
+  HD02_VD01: 'total_pop_err',
+  HD01_VD02: 'white_pop',
+  HD02_VD02: 'white_pop_err',
+  HD01_VD03: 'black_pop',
+  HD02_VD03: 'black_pop_err',
+  HD01_VD04: 'american_indian_pop',
+  HD02_VD04: 'american_indian_pop_err',
+  HD01_VD05: 'asian_pop',
+  HD02_VD05: 'asian_pop_err',
+  HD01_VD06: 'pacific_islander_pop',
+  HD02_VD06: 'pacific_islander_pop_err',
+  HD01_VD07: 'other_pop',
+  HD02_VD07: 'other_pop_err',
+  HD01_VD08: 'two_or_more_pop',
+  HD02_VD08: 'two_or_more_pop_err',
+  HD01_VD09: 'two_or_more_other_pop',
+  HD02_VD09: 'two_or_more_other_pop_err',
+  HD01_VD10: 'two_or_more_except_pop',
+  HD02_VD10: 'two_or_more_except_pop_err'
+};
---
+++ test/pointifyBlocks.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var expect = require('chai').expect;
+var geojsonhint = require('geojsonhint');
+var pointify = require('../lib/pointify');
+
+var sampleFeatures = require('./fixtures/block_groups_with_acs');
+
+var valueProps = [
+  'HD01_VD02',
+  'HD01_VD03',
+  'HD01_VD04',
+  'HD01_VD05',
+  'HD01_VD06',
+  'HD01_VD07',
+  'HD01_VD08',
+  'HD01_VD09',
+  'HD01_VD010'
+];
+
+describe('pointify', function() {
+  it('should create random points with individual value properties within each block group', function(done) {
+    pointify(sampleFeatures, valueProps, function(err, data) {
+      var after = _.after(data.length, function() {
+        return done(err);
+      });
+
+      expect(data).to.be.an('array');
+      expect(data.length).to.be.above(16000);
+
+      return _.map(data, function(item) {
+        expect(geojsonhint.hint(item)).to.be.empty;
+        expect(item.properties).to.have.property('race');
+        return after();
+      });
+    });
+  });
+});
